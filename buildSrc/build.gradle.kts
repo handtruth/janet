843cc81493e3efd0c9d5693514aebbbf22d2084f
ktlo <ktlo@handtruth.com>
2024-06-07 07:34:50 +0300

    Fix jgit security issue

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     implementation("com.gladed.androidgitversion:gradle-android-git-version:0.4.14")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
 }
